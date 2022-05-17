7b10dcaf107bcc5a7d289e50f4396c18e303d8b9
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-05-17 01:11:05 -0500

    Se agrego funcionalida de busqueda

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,21 +1,36 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Spinner } from "../components/Spinner";
 import { get } from "../utils/httpClient";
 import movie from "./movie.json";
 import styles from "./MovieDetails.module.css"
+import {useQuery} from "../hooks/useQuery.jsx"
 
 export function MovieDetails(){
     const {movieId} = useParams();
+    const [isLoading, setIsLoading] = useState(true);
     const [movie, setMovie] = useState(null);
-    console.log(movieId);
 
+    const query = useQuery();
+    const search = query.get("search"); 
+    // console.log(movieId);
+
+
+    
 
     useEffect(() =>{
+        setIsLoading(true);
         get("/movie/" + movieId).then((data)=>{
+            
             setMovie(data);
+            setIsLoading(false);
         })
 
     }, [movieId]);
+
+    if(isLoading){
+        return <Spinner />;
+    }
 
     if(!movie){
         return null;
